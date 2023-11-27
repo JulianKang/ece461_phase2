cb24564d7c20c8ede931da63c61ca83b4b550607
Ben Boardley <bboard1202@gmail.com>
2023-11-27 15:44:14 -0500

    change aws scripts

--- aws/scripts/application-stop.sh
+++ aws/scripts/application-stop.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Assuming your TypeScript application is running as a Node.js service.
-# You might need to customize this based on how your application is started.
-
-# Find the process ID (PID) of your TypeScript application
-#!/bin/bash
-
 # Log file path
 log_file="/home/ubuntu/pm2.logs"
 
@@ -15,6 +9,14 @@ env >> "$log_file"
 # Navigate to the application directory
 cd /home/ubuntu/apps/ece461_phase2
 
-# Stop PM2 processes and capture output to log file
-pm2 stop all >> "$log_file" 2>&1
-pm2 delete all >> "$log_file" 2>&1
+# Get the number of online processes without a newline
+online_processes=$(pm2 list | grep -c 'online')
+
+# Check if there are any online processes
+if [ "$online_processes" -gt 0 ]; then
+    # Stop PM2 processes and capture output to log file
+    pm2 stop all >> "$log_file" 2>&1
+    pm2 delete all >> "$log_file" 2>&1
+else
+    echo "No online processes to stop and delete." >> "$log_file"
+fi
