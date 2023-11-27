c232b74c4bb8cb083a7b8cb21eb1548e59809f1b
Ben Boardley <bboard1202@gmail.com>
2023-11-27 14:03:52 -0500

    change appspec

---
+++ dist/schemas.js
@@ -0,0 +1,13 @@
+"use strict";
+// for consistency: 
+// import * as Schemas from './schemas';
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Actions = void 0;
+// used in package history, currently out of scope
+var Actions;
+(function (Actions) {
+    Actions["CREATE"] = "CREATE";
+    Actions["UPDATE"] = "UPDATE";
+    Actions["DOWNLOAD"] = "DOWNLOAD";
+    Actions["RATE"] = "RATE";
+})(Actions || (exports.Actions = Actions = {}));
