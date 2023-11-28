f558c806c9e22ae7cdaf4e67783eea4ffe3d819b
Ben Boardley <bboard1202@gmail.com>
2023-11-28 05:47:31 -0500

    test CD with RDS

--- src/Buttons.tsx
+++ src/Buttons.tsx
@@ -4,7 +4,7 @@ function Buttons() {
     return (
         <div className="buttons-container">
             <button className="button sign-in" onClick={() => window.location.href = 'signIn'}>Sign In</button>
-            <button className="button register" onClick={() => window.location.href = 'register'}>Register</button>
+            <button className="button register" onClick={() => window.location.href = 'register'}>Register User</button>
         </div>
     );
 }
