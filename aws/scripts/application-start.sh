b911ba06711a5c06c193543a6b981b73c08359c0
Ben Boardley <bboard1202@gmail.com>
2023-12-05 13:22:06 -0500

    updated aws script

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -4,6 +4,6 @@ set -xe
 cd /home/ubuntu/apps
 
 # Start serve with the obtained public IP and desired port
-pm2 start dist/server/server.js
+pm2 start dist/server/start_server.js
 service nginx restart
 echo "complete"
